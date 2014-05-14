7a123ade4f1f987991e01058f32782de717724cb
chabbi.milind@gmail.com <chabbi.milind@gmail.com@5f9c7a29-777f-c911-3e27-df0ae3a3588b>
2014-05-14 20:06:37 +0000

    code for performance evaluation

--- trunk/experiments/dmapp_overrides/grace_build.sh
+++ trunk/experiments/dmapp_overrides/grace_build.sh
@@ -1,10 +1,10 @@
 set -ex
-CFLAGS=-O3
+CFLAGS=-O0
 CC -c -g $CFLAGS barrier_deletion.cpp -I//global/u2/m/mc29/software/libunwind-1.1-install/include -fno-omit-frame-pointer
 #gcc -E -B -c test.c -o dmapp_wrapped.E -I/opt/cray/dmapp/7.0.1-1.0501.8315.8.4.ari/include  -I/opt/cray/gni-headers/3.0-1.0501.8317.12.1.ari/include
 CC -E -B -c test.c -o dmapp_wrapped.E 
 python dmapp-wrapper.py dmapp_wrapped.E  dmapp_wrapped.c  wrap_scrpt
 CC -c -g $CFLAGS dmapp_wrapped.c -o dmapp_wrapped.o 
-cd ~/nwchem-6.3_opt/src/
+cd ~/nwchem-6.3_grace/src/
 sh build_barrier_deletion.sh
 cd -
