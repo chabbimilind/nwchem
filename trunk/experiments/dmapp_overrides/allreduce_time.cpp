89d7c4b4cd7c6cf345a98aaa5a612b96c568473a
chabbi.milind@gmail.com <chabbi.milind@gmail.com@5f9c7a29-777f-c911-3e27-df0ae3a3588b>
2014-05-16 21:12:58 +0000

    beautified the code

--- trunk/experiments/dmapp_overrides/allreduce_time.cpp
+++ trunk/experiments/dmapp_overrides/allreduce_time.cpp
@@ -57,29 +57,29 @@ using namespace std;
 using namespace std::tr1;
 
 extern "C" {
-    
+
 #define BARRIER_FN_NAME "MPI_Barrier"
 #define ALLGATHER_FN_NAME "MPI_Allgather"
 #define ALLREDUCE_FN_NAME "MPI_Allreduce"
-    
+
 #define REAL_FUNCTION(name)  __real_ ## name
 #define WRAPPED_FUNCTION(name)  __wrap_ ## name
-    
+
 #define ALL_REDUCE_BUFFER(status) ( ((status) << 32 ) | GLOBAL_STATE.GetBarrierInstance())
 #define ALL_REDUCE_GET_STATUS(buffer) ( (buffer) >> 32 )
 #define ALL_REDUCE_GET_INSTANCE(buffer) ((buffer) & 0xffffffff)
-    
-    
+
+
 #define SKIP (10)
 #define PARTICIPATE (0)
-    
+
 //#define ENABLE_LOGGING
 
-    
+
 //#define VERBOSE
-    
+
     /******** Globals variables **********/
-    
+
     struct GLOBAL_STATE_t {
         unordered_map<uint64_t, uint64_t> barrierSkipCache;
         unordered_map<uint64_t, uint64_t>::iterator barrierSkipCacheIterator;
@@ -90,162 +90,193 @@ extern "C" {
         uint64_t badDecison;
         uint64_t lastParticipatedBarrier;
         bool isEnabled;
-        
-        void Enable() { isEnabled = true;}
-        void Disable() { isEnabled = false;}
-        bool IsEnabled() { return isEnabled;}
 
-        
-        void SetLastParticipatedBarrier( uint64_t lpb) { lastParticipatedBarrier = lpb; }
-        uint64_t GetLastParticipatedBarrier() { return lastParticipatedBarrier;}
-        
-        uint64_t IncrementBadDecision() { return badDecison++;}
-        uint64_t GetBadDecision() { return badDecison;}
-        
-        uint64_t IncrementSkippable() {return skippable ++;}
-        uint64_t DecrementSkippable() {return skippable --;}
-        uint64_t GetSkippable() {return skippable;}
-        
-        uint64_t IncrementReSync() {return reSync ++;}
-        uint64_t GetReSync() { return reSync;}
-        
+        void Enable() {
+            isEnabled = true;
+        }
+        void Disable() {
+            isEnabled = false;
+        }
+        bool IsEnabled() {
+            return isEnabled;
+        }
+
+
+        void SetLastParticipatedBarrier(uint64_t lpb) {
+            lastParticipatedBarrier = lpb;
+        }
+        uint64_t GetLastParticipatedBarrier() {
+            return lastParticipatedBarrier;
+        }
+
+        uint64_t IncrementBadDecision() {
+            return badDecison++;
+        }
+        uint64_t GetBadDecision() {
+            return badDecison;
+        }
+
+        uint64_t IncrementSkippable() {
+            return skippable ++;
+        }
+        uint64_t DecrementSkippable() {
+            return skippable --;
+        }
+        uint64_t GetSkippable() {
+            return skippable;
+        }
+
+        uint64_t IncrementReSync() {
+            return reSync ++;
+        }
+        uint64_t GetReSync() {
+            return reSync;
+        }
+
         uint64_t IncrementEnabledBarrierInstance() {
             uint64_t val = enabledBarrierInstance;
-            if (IsEnabled())
+
+            if(IsEnabled())
                 enabledBarrierInstance ++;
+
             return val;
         }
-        uint64_t GetEnabledBarrierInstance() { return enabledBarrierInstance;}
-        
-        uint64_t IncrementBarrierInstance() { IncrementEnabledBarrierInstance(); return barrierInstance ++;}
-        uint64_t GetBarrierInstance() {return barrierInstance;}
+        uint64_t GetEnabledBarrierInstance() {
+            return enabledBarrierInstance;
+        }
+
+        uint64_t IncrementBarrierInstance() {
+            IncrementEnabledBarrierInstance();
+            return barrierInstance ++;
+        }
+        uint64_t GetBarrierInstance() {
+            return barrierInstance;
+        }
         bool doAllReduce;
-        
-        
+
+
     } ;
     GLOBAL_STATE_t GLOBAL_STATE;
-    
+
     static int myRank = -1;
     static MPI_Op myMPIOp;
-    
-    static void PrintStats(){
-        if(myRank == 0){
+
+    static void PrintStats() {
+        if(myRank == 0) {
             printf("\n Total Barriers = %lu, Enabled = %lu, Skippable =%lu, reSync = %lu, bad decision = %lu", GLOBAL_STATE.GetBarrierInstance(), GLOBAL_STATE.GetEnabledBarrierInstance(), GLOBAL_STATE.GetSkippable(), GLOBAL_STATE.GetReSync(), GLOBAL_STATE.GetBadDecision());
         }
     }
 
-    
-    static void MyMPIReductionOp(void * a, void * b, int * len, MPI_Datatype * type) {
+
+    static void MyMPIReductionOp(void* a, void* b, int* len, MPI_Datatype* type) {
         uint64_t a1 = *((uint64_t*)a);
         uint64_t b1 = *((uint64_t*)b);
         uint32_t statusA = ALL_REDUCE_GET_STATUS(a1);
         uint32_t statusB = ALL_REDUCE_GET_STATUS(b1);
-        
         uint32_t instanceA = ALL_REDUCE_GET_INSTANCE(a1);
         uint32_t instanceB = ALL_REDUCE_GET_INSTANCE(b1);
-        
         // Min of status
         uint64_t retStatus = statusA < statusB ? statusA : statusB;
         // Min of barrier instance
         uint64_t retInstance = instanceA < instanceB ? instanceA : instanceB;
-        *((uint64_t *)b) =  (((retStatus) << 32 ) | retInstance);
+        *((uint64_t*)b) = (((retStatus) << 32) | retInstance);
     }
-    
-    
-    
-    extern int REAL_FUNCTION(MPI_Barrier) (MPI_Comm comm);
-    extern int REAL_FUNCTION(MPI_Init) (int *argc, char ***argv);
-    extern int REAL_FUNCTION(MPI_Finalize) (void);
-    
-    static inline void ParticipateInBarrier(MPI_Comm comm, uint64_t key, uint64_t curBarrierInstance, uint64_t val, int &retVal){
+
+
+
+    extern int REAL_FUNCTION(MPI_Barrier)(MPI_Comm comm);
+    extern int REAL_FUNCTION(MPI_Init)(int* argc, char** *argv);
+    extern int REAL_FUNCTION(MPI_Finalize)(void);
+
+    static inline void ParticipateInBarrier(MPI_Comm comm, uint64_t key, uint64_t curBarrierInstance, uint64_t val, int& retVal) {
         uint64_t recvBuf;
         uint64_t sendBuf = PARTICIPATE;
         sendBuf = ALL_REDUCE_BUFFER(sendBuf);
         retVal = MPI_Allreduce(&sendBuf, &recvBuf, 1, MPI_UNSIGNED_LONG, myMPIOp, comm);
     }
-    
+
     static struct timeval t1, t2;
-    
-    #define TIME_SPENT(start, end) (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec*1000000 - start.tv_usec)
-    
-    static void EnableBarrierOptimization(){
+
+#define TIME_SPENT(start, end) (end.tv_sec * 1000000 + end.tv_usec - start.tv_sec*1000000 - start.tv_usec)
+
+    static void EnableBarrierOptimization() {
         if(myRank == 0) {
-            gettimeofday(&t1,NULL);
+            gettimeofday(&t1, NULL);
             printf("\n Enabled BO\n");
         }
+
         GLOBAL_STATE.Enable();
     }
     // fortran interface
-    void enable_barrier_optimization_(){
+    void enable_barrier_optimization_() {
         EnableBarrierOptimization();
     }
-    
-    static void DisableAndCleanupBarrierOptimization(){
+
+    static void DisableAndCleanupBarrierOptimization() {
         if(myRank == 0) {
             printf("\n Disabled BO\n");
         }
+
         GLOBAL_STATE.Disable();
     }
-    
+
     // fortran interface
-    void disable_and_cleanup_barrier_optimization_(){
+    void disable_and_cleanup_barrier_optimization_() {
         DisableAndCleanupBarrierOptimization();
     }
-    
-    static void DisableBarrierOptimization(){
+
+    static void DisableBarrierOptimization() {
         if(myRank == 0) {
-            gettimeofday(&t2,NULL);
+            gettimeofday(&t2, NULL);
             uint64_t span = TIME_SPENT(t1, t2);
             printf("\n Disabled BO %lu \n", span);
         }
+
         GLOBAL_STATE.Disable();
     }
-    
+
     // fortran interface
-    void disable_barrier_optimization_(){
+    void disable_barrier_optimization_() {
         DisableBarrierOptimization();
     }
-    
 
-    
-    int WRAPPED_FUNCTION(MPI_Barrier) (MPI_Comm comm){
+
+
+    int WRAPPED_FUNCTION(MPI_Barrier)(MPI_Comm comm) {
         if(!GLOBAL_STATE.IsEnabled() || !GLOBAL_STATE.doAllReduce)
             return  REAL_FUNCTION(MPI_Barrier)(comm);
-        
+
         int retVal = MPI_SUCCESS;
         // increment the barrrier instance
         uint64_t curBarrierInstance = GLOBAL_STATE.IncrementBarrierInstance();
-        
         // Enabled, hence perform tracking / optimization
-        
-
         ParticipateInBarrier(comm, 0, curBarrierInstance, 0, retVal);
         return retVal;
     }
-    
-    
-    int WRAPPED_FUNCTION(MPI_Init) (int *argc, char ***argv) {
+
+
+    int WRAPPED_FUNCTION(MPI_Init)(int* argc, char** *argv) {
         int retVal = REAL_FUNCTION(MPI_Init)(argc, argv);
         // Register my reduction op
         MPI_Op_create(MyMPIReductionOp, 1 /*commute*/, &myMPIOp);
         atexit(PrintStats);
-        MPI_Comm_rank(MPI_COMM_WORLD, &myRank );
-        
+        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
         // Read env whether to do all reduce or barrier
-        
-        char * val = getenv("NWCHEM_BARRIER_TYPE");
+        char* val = getenv("NWCHEM_BARRIER_TYPE");
+
         if(val) {
-            if (strcmp(val,"allreduce") == 0 ) {
+            if(strcmp(val, "allreduce") == 0) {
                 GLOBAL_STATE.doAllReduce = true;
             } else {
                 GLOBAL_STATE.doAllReduce = false;
             }
         }
+
         return retVal;
     }
 
-    
-    int WRAPPED_FUNCTION(MPI_Finalize) () {
+
+    int WRAPPED_FUNCTION(MPI_Finalize)() {
         int retVal = REAL_FUNCTION(MPI_Finalize)();
         return retVal;
     }
