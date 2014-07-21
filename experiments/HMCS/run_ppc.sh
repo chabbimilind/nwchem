349eed4f27fa3d194fe15b83436c56eaa0c60088
chabbi.milind <chabbi.milind@gmail.com>
2014-07-21 21:33:28 +0000

    HMCS: Converted recursive acquire into a loop to avoid the cost of return from recursion, this reduces the critical path length

--- experiments/HMCS/run_ppc.sh
+++ experiments/HMCS/run_ppc.sh
@@ -11,3 +11,7 @@ time ./hmcs_ppc $nIter 12 3 6 $level1TH 12  $level2TH 12 $level3TH
 time ./hmcs_ppc $nIter 24 3 6 $level1TH 12  $level2TH 24 $level3TH
 time ./hmcs_ppc $nIter 36 3 6 $level1TH 12  $level2TH 36 $level3TH
 time ./hmcs_ppc $nIter 48 3 6 $level1TH 12  $level2TH 48 $level3TH
+for i in {1..9999}
+do
+time ./hmcs_ppc $nIter 48 5 2 10 6 10 12 5 24 5 48 1
+done
