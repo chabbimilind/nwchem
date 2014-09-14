58638fcd0bb262da4a0b353e8b36b6d0448ee98c
chabbi.milind <chabbi.milind@5f9c7a29-777f-c911-3e27-df0ae3a3588b>
2014-09-14 19:36:01 +0000

    HMCS:Added fairness simulation

---
+++ experiments/HMCS/fairness_simulation/HMCS_fairness_simulation.h
@@ -0,0 +1,15 @@
+//
+//  HMCS_fairness_simulation.h
+//  
+//
+//  Created by Milind Chabbi on 8/19/14.
+//
+//
+
+#ifndef ____HMCS_fairness_simulation__
+#define ____HMCS_fairness_simulation__
+
+#include <iostream>
+
+#endif /* defined(____HMCS_fairness_simulation__) */
+
