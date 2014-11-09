d2090975585be1031d3de8194a5664946da7ab97
chabbi.milind <chabbi.milind@gmail.com>
2014-11-09 17:08:20 +0000

    HMCS: non working version of 3 level lock. root level works, but assers for 3 level

--- experiments/HMCS/spin/run.sh
+++ experiments/HMCS/spin/run.sh
@@ -2,11 +2,10 @@ set -ex
 S=/Users/mc29/softwares/Spin/Src6.4.2/
 rm -f abort.pml.trail
 rm -f pan.*
-$S/spin  abort.pml
+#$S/spin  abort.pml
 $S/spin -a  abort.pml
-gcc -O3 -DVECTORSZ=100 -DSAFETY -DCOLLAPSE  -o pan pan.c
+gcc -O3 -DVECTORSZ=9999 -DSAFETY -DCOLLAPSE  -o pan pan.c
 #gcc  -O3 -DBITSTATE -DVECTORSZ=9999 -DSAFETY -o pan pan.c
 #gcc -g -O3 -DVECTORSZ=53000 -DCOLLAPSE -DSAFETY -DMEMLIM=10000 -DNCORE=2 pan.c -o pan
-./pan
+./pan 
 $S/spin -t  -l -p abort.pml
-
