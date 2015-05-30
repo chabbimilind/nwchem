2ace761102b4f08f9010b435a1103321a0f8f8c6
chabbi.milind <chabbi.milind@5f9c7a29-777f-c911-3e27-df0ae3a3588b>
2015-05-30 17:55:44 +0000

    AHMCS: contention texts fixup

--- experiments/HMCS/adaptive/tests/run_adapt.sh
+++ experiments/HMCS/adaptive/tests/run_adapt.sh
@@ -19,20 +19,26 @@ level3TH=1
 #time ./hmcs_ppc $timeout $nIter 36 3 6 $level1TH 12  $level2TH 36 $level3TH
 #time ./hmcs_ppc $timeout $nIter 48 3 6 $level1TH 12  $level2TH 48 $level3TH
 #for i in 0 100 500 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2100 2200 2300 2400 2500 2700 3000 4000 5000 5100 5200 5300 5400 5500 5600 5700 5800 5900 6000 6100 6200 6300 6400 6500 6700 7000 7200 7500 8000 9000 10000
-#for i in 2 4
-for i in 4 
+for i in 2 4
+#for i in 4 
 do
-#for j in 1 10 100
-for j in 1
+for j in 1 10 100
+#for j in 1
 do
 $CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_ppc hmcs_ppc_latency.cpp -lrt  &
-$CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_adapt_counter_based hmcs_adapt_counter_based.cpp -lrt  &
+$CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_ppc_latency_slow_fast hmcs_ppc_latency_slow_fast.cpp -lrt  &
+$CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_adapt_eager_no_fastpath hmcs_adapt_eager_no_fastpath.cpp -lrt  &
+$CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_adapt_lazy_no_fastpath hmcs_adapt_lazy_no_fastpath.cpp -lrt  &
+$CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_adapt_counter_based_check_cur_first hmcs_adapt_counter_based_check_cur_first.cpp -lrt  &
 $CXX -DHOW_LONG_MS=$j -DMAX_DATA=$i -DNITER=4 $FLAGS -DTHE_WAIT="0" -o hmcs_adapt_instantaneous_slow_fast_reuse_next hmcs_adapt_instantaneous_slow_fast_reuse_next.cpp -lrt  &
 wait 
 time ./hmcs_ppc $mustBeAMultiple $timeout $nIter 128 1 128 $level3TH
 time ./hmcs_ppc $mustBeAMultiple $timeout $nIter 128 2 32  $level2TH 128 $level3TH
 time ./hmcs_ppc $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
-time ./hmcs_adapt_counter_based $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
+time ./hmcs_ppc_latency_slow_fast $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
+time ./hmcs_adapt_eager_no_fastpath $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
+time ./hmcs_adapt_lazy_no_fastpath $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
 time ./hmcs_adapt_instantaneous_slow_fast_reuse_next $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
+time ./hmcs_adapt_counter_based_check_cur_first $mustBeAMultiple $timeout $nIter 128 3 4 $level1TH 32  $level2TH 128 $level3TH
 done
 done
